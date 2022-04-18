8a6a8e27840e89e155e190b2c6fef5feebfed1fa
Bradley Herrera Contreras <bradleyhrct@gmail.com>
2022-04-18 00:07:05 -0400

    Revert

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/bradleyherctt/Documents/GitHub/bradleyhrc.github.io/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/bradleyherctt/Documents/GitHub/bradleyhrc.github.io/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/bradleyherctt/Documents/GitHub/bradleyhrc.github.io/src/pages/index.js")))
 }
